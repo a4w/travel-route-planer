ceaf5b331590d269ff20fef29eb774918de54760
Ahmed Wessam <ahmed.wessam.1999@gmail.com>
2020-05-17 14:56:18 +0200

    Add dijsktra algorithm

---
+++ GraphAlgorithms.py
@@ -0,0 +1,42 @@
+import Node
+from queue import PriorityQueue
+
+
+class GraphAlgorithms():
+
+    @staticmethod
+    def dijsktra(startNode: Node, endNode: Node) -> dict:
+        upcoming = PriorityQueue()
+        # We add a tuple, first element is the cost, the second is the node
+        upcoming.put((0, startNode))
+
+        # To store path to source
+        parent = dict()
+
+        # To store minimum cost from startNode to a given node
+        costs = dict()
+        costs[startNode] = 0
+
+        while not upcoming.empty():
+            current: Node = upcoming.get()[1]
+
+            # Check if we found the destination
+            if(current == endNode):
+                return parent
+
+            for adj in current.neighbours:
+                # Adjacent node
+                node: Node = adj[0]
+                # Edge cost
+                cost: int = adj[1]
+
+                # New cost to neighbour
+                newCost: int = costs[current] + cost
+
+                if(node not in costs or newCost < costs[node]):
+                    parent[node] = current
+                    costs[node] = newCost
+                    upcoming.put((newCost, node))
+
+        # No path found
+        raise Exception("No path between source and destination found")
