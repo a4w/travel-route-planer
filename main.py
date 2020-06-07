558d9fd6f399ce8161a70d310aed696e7fbd442e
Ahmed Wessam <ahmed.wessam.1999@gmail.com>
2020-06-07 10:35:50 +0200

    Remove unneeded function

--- main.py
+++ main.py
@@ -1,20 +1,6 @@
 from FlightKnowledgeExtractor import FlightKnowledgeExtractor
 from TravelRoutePlanner import TravelRoutePlanner
 
-
-def isWithinDay(subject, start, end):
-    if(start <= end):
-        return subject >= start and subject <= end
-    else:
-        return subject <= start and subject >= end
-
-
-def printPath(parent, end):
-    if(end in parent):
-        printPath(parent, parent[end])
-        print(str(parent[end].value) + '->' + str(end.value))
-
-
 kb = FlightKnowledgeExtractor(
     "./knowledge/cities.csv", "./knowledge/timetable.csv")
 cities = kb.getCities()
