0c9c8c4c56366f052b13558412d1f26e76983d01
Ahmed Wessam <ahmed.wessam.1999@gmail.com>
2020-06-07 10:40:32 +0200

    Add flight number

--- Flight.py
+++ Flight.py
@@ -3,12 +3,13 @@ from Timestamp import Timestamp
 
 
 class Flight:
-    def __init__(self, sourceCity: City, destinationCity: City, departure: Timestamp, arrival: Timestamp):
+    def __init__(self, sourceCity: City, destinationCity: City, departure: Timestamp, arrival: Timestamp, flightNumber: str):
         self.sourceCity = sourceCity
         self.destinationCity = destinationCity
         self.departure = departure
         self.arrival = arrival
+        self.flightNumber = flightNumber
 
     def __str__(self):
-        return "From " + str(self.sourceCity) + " to " + str(self.destinationCity) + ", departure: " + str(self.departure) + ", arrival: " + str(self.arrival)
+        return self.flightNumber + " From " + str(self.sourceCity) + " to " + str(self.destinationCity) + ", departure: " + str(self.departure) + ", arrival: " + str(self.arrival)
 
