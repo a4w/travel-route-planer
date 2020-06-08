6d61014e6a047a0750d264dcaf63680dc5c57968
Ahmed Wessam <ahmed.wessam.1999@gmail.com>
2020-06-08 04:57:53 +0200

    Removed visualization from knowledge extractor

--- FlightKnowledgeExtractor.py
+++ FlightKnowledgeExtractor.py
@@ -1,7 +1,4 @@
 import csv
-import networkx as nx
-import matplotlib.pyplot as plt
-import time
 from collections import defaultdict
 from City import City
 from Flight import Flight
@@ -10,40 +7,9 @@ from Timestamp import Timestamp
 
 class FlightKnowledgeExtractor:
 
-    def updateGraph(self, wait=0.5):
-        time.sleep(wait)
-        plt.clf()
-        colors = [self.graph.nodes[name]["node_color"]
-                  for name in self.graph.nodes()]
-        pos = nx.get_node_attributes(self.graph, 'pos')
-        nx.draw_networkx(self.graph, with_labels=True,
-                         node_color=colors, pos=pos)
-        plt.draw()
-        plt.pause(0.01)
-
-    def resetGraph(self):
-        for n in self.graph.nodes():
-            self.graph.nodes[n]["node_color"] = '#CCCCCC'
-
-    def visitGraphNode(self, city, end=False):
-        if(end):
-            self.graph.nodes[city]["node_color"] = "#000000"
-        else:
-            self.graph.nodes[city]["node_color"] = "#0095ff"
-
-        self.updateGraph()
-
-    def markAvailableGraphNode(self, city):
-        self.graph.nodes[city]["node_color"] = "#00FF00"
-        self.updateGraph()
-
-    def closeVisualizer(self):
-        plt.close()
-
     def __init__(self, citiesFile, timetableFile):
         # Read cities and save them in list
         self.cities = dict()
-        self.graph = nx.Graph()
         with open(citiesFile) as csvDataFile:
             csvReader = csv.reader(csvDataFile)
             for row in csvReader:
@@ -52,9 +18,6 @@ class FlightKnowledgeExtractor:
                 long = row[2]
                 self.cities[name] = (
                     City(name, float(lat), float(long)))
-                self.graph.add_node(name, pos=(float(lat), float(long)))
-                self.graph.nodes[name]["node_color"] = '#CCCCCC'
-                self.updateGraph(0)
 
         # self.flights = list()
         with open(timetableFile) as csvDataFile:
